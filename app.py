a14b89575f931040c4b1c00d0261a32991d87bfb
garryduff <99087011+garryduff@users.noreply.github.com>
2023-02-15 07:08:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder='/templates')
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
