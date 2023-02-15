1afe5a50ba04a8e1a29e92827a750eafda7b6453
garryduff <99087011+garryduff@users.noreply.github.com>
2023-02-15 06:34:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-import openai
-from flask import Flask, render_template, request
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# Set up OpenAI API credentials
-openai.api_key = "YOUR_API_KEY"
+@app.route('/calculate')
+def calculate():
+    x = request.args.get('x')
+    if x is None:
+        return "Please provide a value for x."
+    try:
+        x = int(x)
+    except ValueError:
+        return "Invalid value for x: must be an integer."
+    result = 3 * x
+    return f"The result is {result}."
 
-# Define the GPT-3 model
-model_engine = "davinci"
-
-# Route to display input form
-@app.route("/")
-def index():
-    return render_template("input.html")
-
-# Route to process form data and generate output
-@app.route("/", methods=["POST"])
-def generate_output():
-    # Retrieve input data from form
-    input_text = request.form["input_text"]
-
-    # Generate output using GPT-3 API
-    prompt = f"Generate some text based on the input: {input_text}"
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    output_text = response.choices[0].text
-
-    # Return output on web page
-    return render_template("output.html", input_text=input_text, output_text=output_text)
+if __name__ == '__main__':
+    app.run()
