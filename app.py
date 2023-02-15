aa2453d20f83f02290b6c1179313b9b1b6960178
garryduff <99087011+garryduff@users.noreply.github.com>
2023-02-15 06:43:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('input.html')
+
 @app.route('/calculate')
 def calculate():
-    x = request.args.get('x')
-    if x is None:
-        return "Please provide a value for x."
-    try:
-        x = int(x)
-    except ValueError:
-        return "Invalid value for x: must be an integer."
-    result = 3 * x
-    return f"The result is {result}."
+    # Get the value of x from the request and calculate 3x
+    # ...
 
 if __name__ == '__main__':
     app.run()
