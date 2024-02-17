678c3a5ed258664e18f3041098c442d7bc846239
garryduff <99087011+garryduff@users.noreply.github.com>
2024-02-17 20:46:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,15 +22,17 @@ def generate_text():
     from openai import OpenAI
     client = OpenAI()
 
-    completion = client.chat.completions.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello!"}
-      ]
+    response = client.completions.create(
+      model="ft:babbage-002:personal::8tJOCt1q",
+      prompt="We develop a tractable model of systemic bank runs. The market-based banking system features a two-layer structure: banks with heterogeneous fundamentals face potential runs by their creditors while they trade short-term funding in the asset (interbank) market in response to creditor withdrawals. The possibility of a run on a particular bank depends on its assets' interim liquidation value, and this value depends endogenously in turn on the status of other banks in the asset market. The within-bank coordination problem among creditors and the cross-bank price externality feed into each other. A small shock can be amplified into a systemic crisis.->> ad yyyyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy",
+      temperature=1,
+      max_tokens=25,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
     )
 
-    message = completion.choices[0].message.content
+    message = response['choices'][0]['text']
     originalquery = message
 
     return render_template('input.html', message=message, originalquery=originalquery) 
