0f6bb1310bc99093a391f39eef207548faabc643
garryduff <99087011+garryduff@users.noreply.github.com>
2024-02-17 21:16:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,14 +24,16 @@ def generate_text():
 
     response = client.completions.create(
       model="ft:babbage-002:personal::8tJOCt1q",
-      prompt="We develop a tractable model of systemic bank runs. The market-based banking system features a two-layer structure: banks with heterogeneous fundamentals face potential runs by their creditors while they trade short-term funding in the asset (interbank) market in response to creditor withdrawals. The possibility of a run on a particular bank depends on its assets' interim liquidation value, and this value depends endogenously in turn on the status of other banks in the asset market. The within-bank coordination problem among creditors and the cross-bank price externality feed into each other. A small shock can be amplified into a systemic crisis.->> ad yyyyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy yyy",
+      prompt="We develop a tractable model of systemic bank runs. The market-based banking system features a two-layer structure: banks with heterogeneous fundamentals face potential runs by their creditors while they trade short-term funding in the asset (interbank) market in response to creditor withdrawals. The possibility of a run on a particular bank depends on its assets' interim liquidation value, and this value depends endogenously in turn on the status of other banks in the asset market. The within-bank coordination problem among creditors and the cross-bank price externality feed into each other. A small shock can be amplified into a systemic crisis.->>",
       temperature=1,
-      max_tokens=25,
+      max_tokens=3,
       top_p=1,
       frequency_penalty=0,
-      presence_penalty=0
+      presence_penalty=0,
+      logprobs=3,
+      stop=[" yyy"]
     )
-
+    
     message = response.choices[0].text
     originalquery = message
 
