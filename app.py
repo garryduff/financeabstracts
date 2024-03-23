76f51f976d1d22f732911d7a2bca147cf9bfeaab
garryduff <99087011+garryduff@users.noreply.github.com>
2024-03-23 15:34:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,30 @@ BUCKET_NAME = os.environ.get('herokufinanceapp')  # Replace with your bucket nam
 def account():
     return render_template('account.html')
 
+@app.route('/sign_s3/')
+def sign_s3():
+  S3_BUCKET = os.environ.get('S3_BUCKET')
+
+  file_name = request.args.get('file_name')
+  file_type = request.args.get('file_type')
+
+  s3 = boto3.client('s3')
+
+  presigned_post = s3.generate_presigned_post(
+    Bucket = S3_BUCKET,
+    Key = file_name,
+    Fields = {"acl": "public-read", "Content-Type": file_type},
+    Conditions = [
+      {"acl": "public-read"},
+      {"Content-Type": file_type}
+    ],
+    ExpiresIn = 3600
+  )
+
+  return json.dumps({
+    'data': presigned_post,
+    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+  })
 
 @app.route('/', methods=['POST'])
 def generate_text():
